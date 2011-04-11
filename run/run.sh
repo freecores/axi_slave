463b688522595274c06097b15ca4471d81e63f7d
eyalhoc <eyalhoc@f61f419c-2199-426d-b718-d42528ebdfa6>
2011-04-11 15:46:04 +0000

    added dos batch file for windows

--- run/run.sh
+++ run/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo Starting RobustVerilog axi slave run
-rm -rf out
-mkdir out
-
 ../../../robust ../src/base/axi_slave.v -od out -I ../src/gen -list filelist.txt -listpath -header
 
 echo Completed RobustVerilog axi slave run - results in run/out/
